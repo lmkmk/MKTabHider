b80d6432609d2f73804f472baa387d90680b08f9
MK <mk@users.noreply.github.com>
2026-07-06 02:50:36 +0800

    fix: hide tab items via liquid lens containers

--- Tweak.m
+++ Tweak.m
@@ -2,97 +2,99 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-static void (*orig_setControllers)(id, SEL, NSArray *, id);
+static void (*orig_tabbar_layout)(id, SEL);
+static BOOL mk_is_applying = NO;
 
-static BOOL MKIsBlockedController(id controller) {
-    NSString *className = NSStringFromClass([controller class]);
-    return [className isEqualToString:@"ContactListUI.ContactsController"] ||
-        [className isEqualToString:@"CallListUI.CallListController"];
+static NSInteger MKCompareViewsByMinX(id a, id b, void *context) {
+    CGFloat ax = ((UIView *)a).frame.origin.x;
+    CGFloat bx = ((UIView *)b).frame.origin.x;
+    if (ax < bx) return NSOrderedAscending;
+    if (ax > bx) return NSOrderedDescending;
+    return NSOrderedSame;
 }
 
-static NSArray *MKFilteredControllers(NSArray *controllers, NSInteger *removedBeforeSelectedIndex, NSInteger selectedIndex) {
-    NSMutableArray *filtered = [NSMutableArray arrayWithCapacity:controllers.count];
-    NSInteger removedBefore = 0;
+static UIView *MKFindLiquidLensView(UIView *root) {
+    NSMutableArray<UIView *> *stack = [NSMutableArray arrayWithObject:root];
+    while (stack.count != 0) {
+        UIView *view = [stack lastObject];
+        [stack removeLastObject];
 
-    for (NSUInteger index = 0; index < controllers.count; index++) {
-        id controller = [controllers objectAtIndex:index];
-        if (MKIsBlockedController(controller)) {
-            if ((NSInteger)index < selectedIndex) removedBefore++;
-            continue;
+        if ([NSStringFromClass(view.class) isEqualToString:@"LiquidLens.LiquidLensView"]) {
+            return view;
         }
-        [filtered addObject:controller];
-    }
 
-    if (removedBeforeSelectedIndex) *removedBeforeSelectedIndex = removedBefore;
-    return filtered;
-}
-
-static id MKRemappedSelectedIndex(id selectedIndex, NSInteger removedBeforeSelectedIndex, NSUInteger filteredCount) {
-    if (!selectedIndex || selectedIndex == [NSNull null]) return selectedIndex;
-    if (![selectedIndex respondsToSelector:@selector(integerValue)]) return selectedIndex;
-    if (filteredCount == 0) return selectedIndex;
-
-    NSInteger value = [selectedIndex integerValue] - removedBeforeSelectedIndex;
-    if (value < 0) value = 0;
-    if (value >= (NSInteger)filteredCount) value = (NSInteger)filteredCount - 1;
-    return @(value);
-}
-
-static void hook_setControllers(id self, SEL _cmd, NSArray *controllers, id selectedIndex) {
-    if (![controllers isKindOfClass:[NSArray class]]) {
-        orig_setControllers(self, _cmd, controllers, selectedIndex);
-        return;
-    }
-
-    NSInteger originalSelectedIndex = 0;
-    if (selectedIndex && selectedIndex != [NSNull null] && [selectedIndex respondsToSelector:@selector(integerValue)]) {
-        originalSelectedIndex = [selectedIndex integerValue];
-    }
-
-    NSInteger removedBeforeSelectedIndex = 0;
-    NSArray *filteredControllers = MKFilteredControllers(controllers, &removedBeforeSelectedIndex, originalSelectedIndex);
-    id remappedSelectedIndex = MKRemappedSelectedIndex(selectedIndex, removedBeforeSelectedIndex, filteredControllers.count);
-
-    orig_setControllers(self, _cmd, filteredControllers, remappedSelectedIndex);
-}
-
-static Method MKFindSetControllersMethod(Class cls, SEL *selector) {
-    SEL preferred = NSSelectorFromString(@"setControllers:selectedIndex:");
-    Method method = class_getInstanceMethod(cls, preferred);
-    if (method) {
-        if (selector) *selector = preferred;
-        return method;
-    }
-
-    unsigned int count = 0;
-    Method *methods = class_copyMethodList(cls, &count);
-    Method found = NULL;
-    SEL foundSelector = NULL;
-
-    for (unsigned int index = 0; index < count; index++) {
-        SEL candidate = method_getName(methods[index]);
-        NSString *name = NSStringFromSelector(candidate);
-        if ([name containsString:@"setControllers"] && [name containsString:@"selectedIndex"]) {
-            found = methods[index];
-            foundSelector = candidate;
-            break;
+        for (UIView *subview in view.subviews) {
+            [stack addObject:subview];
         }
     }
+    return nil;
+}
 
-    if (selector) *selector = foundSelector;
-    if (methods) free(methods);
-    return found;
+static UIView *MKUIViewValueForKey(id object, NSString *key) {
+    @try {
+        id value = [object valueForKey:key];
+        if ([value isKindOfClass:[UIView class]]) return value;
+    } @catch (__unused NSException *exception) {
+    }
+    return nil;
+}
+
+static NSMutableArray<UIView *> *MKTabItemViews(UIView *container) {
+    NSMutableArray<UIView *> *items = [NSMutableArray array];
+
+    for (UIView *view in container.subviews) {
+        NSString *className = NSStringFromClass(view.class);
+        if (![className containsString:@"ItemComponent"] || ![className containsString:@"View"]) continue;
+        if (view.bounds.size.width <= 0.0 || view.bounds.size.height <= 0.0) continue;
+        [items addObject:view];
+    }
+
+    [items sortUsingFunction:MKCompareViewsByMinX context:NULL];
+    return items;
+}
+
+static void MKHideFirstTwoItemsInContainer(UIView *container) {
+    NSMutableArray<UIView *> *items = MKTabItemViews(container);
+    if (items.count < 4) return;
+
+    for (NSUInteger index = 0; index < items.count; index++) {
+        UIView *item = [items objectAtIndex:index];
+        BOOL shouldHide = index < 2;
+        item.hidden = shouldHide;
+        item.alpha = shouldHide ? 0.0 : 1.0;
+        item.userInteractionEnabled = !shouldHide;
+    }
+}
+
+static void MKApplyTabHider(UIView *tabBarView) {
+    if (mk_is_applying) return;
+    mk_is_applying = YES;
+
+    UIView *liquidLensView = MKFindLiquidLensView(tabBarView);
+    if (liquidLensView) {
+        UIView *contentView = MKUIViewValueForKey(liquidLensView, @"contentView");
+        UIView *selectedContentView = MKUIViewValueForKey(liquidLensView, @"selectedContentView");
+
+        if (contentView) MKHideFirstTwoItemsInContainer(contentView);
+        if (selectedContentView) MKHideFirstTwoItemsInContainer(selectedContentView);
+    }
+
+    mk_is_applying = NO;
+}
+
+static void hook_tabbar_layout(id self, SEL _cmd) {
+    if (orig_tabbar_layout) orig_tabbar_layout(self, _cmd);
+    MKApplyTabHider((UIView *)self);
 }
 
 __attribute__((constructor))
 static void MKTabHider_init(void) {
-    Class cls = NSClassFromString(@"TabBarUI.TabBarControllerImpl");
+    Class cls = NSClassFromString(@"_TtCC15TabBarComponent15TabBarComponent4View");
     if (!cls) return;
 
-    SEL selector = NULL;
-    Method method = MKFindSetControllersMethod(cls, &selector);
-    if (!method || !selector) return;
+    Method method = class_getInstanceMethod(cls, @selector(layoutSubviews));
+    if (!method) return;
 
-    orig_setControllers = (void *)method_getImplementation(method);
-    method_setImplementation(method, (IMP)hook_setControllers);
+    orig_tabbar_layout = (void *)method_getImplementation(method);
+    method_setImplementation(method, (IMP)hook_tabbar_layout);
 }
